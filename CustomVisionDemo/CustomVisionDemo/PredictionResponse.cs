524cfc39498f71245d56afb7badf1b6087e676d7
Arwin Neil Baichoo <arwinneil@gmail.com>
2018-04-13 00:00:35 +0400

    Added Response Model

---
+++ CustomVisionDemo/CustomVisionDemo/PredictionResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomVisionDemo
+{
+    public class Prediction
+    {
+        public string TagId { get; set; }
+        public string Tag { get; set; }
+        public double Probability { get; set; }
+    }
+
+    public class PredictionResponse
+    {
+        public string Id { get; set; }
+        public string Project { get; set; }
+        public string Iteration { get; set; }
+        public DateTime Created { get; set; }
+        public List<Prediction> Predictions { get; set; }
+    }
+}
