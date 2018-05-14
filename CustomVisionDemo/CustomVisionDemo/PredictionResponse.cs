92838e0d8a79d268321d2f101da0d0a2bd63a605
Arwin Neil Baichoo <arwinneil@gmail.com>
2018-05-15 01:10:35 +0400

    Update Model to API v2.0 Response

--- CustomVisionDemo/CustomVisionDemo/PredictionResponse.cs
+++ CustomVisionDemo/CustomVisionDemo/PredictionResponse.cs
@@ -4,11 +4,24 @@ using System.Text;
 
 namespace CustomVisionDemo
 {
+  
+
+
+
+    public class Region
+    {
+        public double Left { get; set; }
+        public double Top { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+    }
+
     public class Prediction
     {
-        public string TagId { get; set; }
-        public string Tag { get; set; }
         public double Probability { get; set; }
+        public string TagId { get; set; }
+        public string TagName { get; set; }
+        public Region Region { get; set; }
     }
 
     public class PredictionResponse
@@ -16,7 +29,7 @@ namespace CustomVisionDemo
         public string Id { get; set; }
         public string Project { get; set; }
         public string Iteration { get; set; }
-        public DateTime Created { get; set; }
+        public string Created { get; set; }
         public List<Prediction> Predictions { get; set; }
     }
 }
